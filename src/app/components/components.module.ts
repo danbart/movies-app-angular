e99c067686c709934797a99de996c7ad7aa1ab6c
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-07-16 10:32:15 -0600

    created movies poster component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { RatingModule } from 'ng-starrating';
+import { PipesModule } from '../pipes/pipes.module';
+import { MoviesPosterGridComponent } from './movies-poster-grid/movies-poster-grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SlideshowComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SlideshowComponent],
+  declarations: [
+    NavbarComponent,
+    SlideshowComponent,
+    MoviesPosterGridComponent,
+  ],
+  imports: [CommonModule, RouterModule, RatingModule, PipesModule],
+  exports: [NavbarComponent, SlideshowComponent, MoviesPosterGridComponent],
 })
 export class ComponentsModule {}
---
+++ src/app/components/movies-poster-grid/movies-poster-grid.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../interfaces/now-playing';
+
+@Component({
+  selector: 'app-movies-poster-grid',
+  templateUrl: './movies-poster-grid.component.html',
+  styleUrls: ['./movies-poster-grid.component.css'],
+})
+export class MoviesPosterGridComponent implements OnInit {
+  @Input() movies: Movie[] = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
