3c87252de68cce5966ff527b856dfbb85e88505b
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-07-16 16:29:19 -0600

    update click picture

--- src/app/components/movies-poster-grid/movies-poster-grid.component.ts
+++ src/app/components/movies-poster-grid/movies-poster-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Movie } from '../../interfaces/now-playing';
 
 @Component({
@@ -8,7 +9,11 @@ import { Movie } from '../../interfaces/now-playing';
 })
 export class MoviesPosterGridComponent implements OnInit {
   @Input() movies: Movie[] = [];
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  onMovieClick(movie: Movie) {
+    this.router.navigate(['/movie', movie.id]);
+  }
 }
