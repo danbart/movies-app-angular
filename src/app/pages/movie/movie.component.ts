50954dc1c75ca29ce25d5dd8fdb22e73df1e4238
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-07-16 16:31:32 -0600

    update movie page

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieDetail } from '../../interfaces/movie-detail';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  styleUrls: ['./movie.component.css'],
 })
 export class MovieComponent implements OnInit {
-
-  constructor() { }
+  movie: MovieDetail | undefined;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private movieService: MoviesService
+  ) {}
 
   ngOnInit(): void {
+    const { id } = this.activatedRoute.snapshot.params;
+    this.movieService
+      .getMovieDetail(id)
+      .subscribe((movie) => (this.movie = movie));
   }
 
+  onRegresar() {}
 }
