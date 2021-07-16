aba57be013590cad76de4f5545e8b7016e0ddd7c
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-07-16 16:31:15 -0600

    update search page

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../../interfaces/now-playing';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  title: string = '';
+  movies: Movie[] = [];
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private movieService: MoviesService
+  ) {}
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe((params) => {
+      this.title = params.text;
+      this.movieService
+        .SearchMovie(params.text)
+        .subscribe((movies) => (this.movies = movies));
+    });
   }
-
 }
