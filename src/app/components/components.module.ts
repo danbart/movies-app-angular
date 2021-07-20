3280d7c8c2412a930367fd21df6667da0d91dd98
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2021-07-20 17:24:04 -0600

    update module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RatingModule } from 'ng-starrating';
 import { PipesModule } from '../pipes/pipes.module';
+import { CastSlideshowComponent } from './cast-slideshow/cast-slideshow.component';
 import { MoviesPosterGridComponent } from './movies-poster-grid/movies-poster-grid.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
@@ -12,8 +13,14 @@ import { SlideshowComponent } from './slideshow/slideshow.component';
     NavbarComponent,
     SlideshowComponent,
     MoviesPosterGridComponent,
+    CastSlideshowComponent,
   ],
   imports: [CommonModule, RouterModule, RatingModule, PipesModule],
-  exports: [NavbarComponent, SlideshowComponent, MoviesPosterGridComponent],
+  exports: [
+    NavbarComponent,
+    SlideshowComponent,
+    MoviesPosterGridComponent,
+    CastSlideshowComponent,
+  ],
 })
 export class ComponentsModule {}
